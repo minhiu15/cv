9ff2608e6797a3dbacbabe2596e0b84ccb6ccf11
keikooo <mmhieu.ag@gmail.com>
2026-05-30 12:48:46 +0700

    update ui

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ document.querySelectorAll('.education-header').forEach(header => {
 
   });
 });
+
+const themeBtn = document.getElementById("themeBtn");
+
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme === "light") {
+  document.body.classList.add("light-mode");
+  themeBtn.textContent = "☀️ Light";
+}
+
+themeBtn.addEventListener("click", () => {
+
+  document.body.classList.toggle("light-mode");
+
+  const isLight =
+      document.body.classList.contains("light-mode");
+
+  localStorage.setItem(
+      "theme",
+      isLight ? "light" : "dark"
+  );
+
+  themeBtn.textContent =
+      isLight ? "☀️ Light" : "🌙 Dark";
+});
